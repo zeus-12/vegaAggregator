c6877f4b4fb82369a01ab2fc3435cf2a54374fc8
Firoz-PS <ce19b048@smail.iitm.ac.in>
2021-09-06 00:42:01 +0530

    No new updates, but unused functions are removed from route, service and controller. Swagger for the new route structure needs to be updated

--- resources/acceleron-server/routes/SummaryRoute.js
+++ resources/acceleron-server/routes/SummaryRoute.js
@@ -10,27 +10,4 @@ router.get("/:summaryType", async function (req, res, next) {
   }
 });
 
-router.get("/filterby/:filterName", async function (req, res, next) {
-  try {
-    const data = await new SummaryController(req).fetchSummaryByFilter();
-    return await new BaseResponse(ResponseType.SUCCESS).send(res, data);
-  } catch (error) {
-    return next(error);
-  }
-});
-
-router.get(
-  "/filterby/:filterName/splitby/:filterParameter",
-  async function (req, res, next) {
-    try {
-      const data = await new SummaryController(
-        req
-      ).fetchSummaryByFilterDetailed();
-      return await new BaseResponse(ResponseType.SUCCESS).send(res, data);
-    } catch (error) {
-      return next(error);
-    }
-  }
-);
-
 module.exports = router;
